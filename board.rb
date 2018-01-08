1c0a7475311784e3579724ec685728e68a802d94
Gregory Barrett <gbarrett15@gmail.com>
2018-01-08 13:54:29 +0100

    can make two queens and they can move

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 class Board
+  
   def initialize
     @square_numbers=[0, 1, 2, 3, 4, 5, 6, 7,
           10, 11, 12, 13, 14, 15, 16, 17,
@@ -14,41 +15,41 @@ class Board
     @square_numbers
   end
 
-  def create_grid
-    @hash=Hash.new
+  def create_board
+    @board=Hash.new
     @square_numbers.each do |x|
-      @hash[x]="   "
+      @board[x]="   "
     end
   end
 
-  def hash
-    @hash
+  def board
+    @board
   end
 #this method fills a few squares
 # she can enter a ful square but not pass it
   def test_hash(full)
     full.each do |z|
-      @hash[z]=" F "
+      @board[z]=" F "
     end
   end
 
-  def show_hash
+  def show_board
     puts "   _______________________________"
-    puts "7 |#{@hash.fetch_values(70, 71, 72, 73, 74, 75, 76, 77).join("|")}|"
+    puts "7 |#{@board.fetch_values(70, 71, 72, 73, 74, 75, 76, 77).join("|")}|"
     puts "   _______________________________"
-    puts "6 |#{@hash.fetch_values(60, 61, 62, 63, 64, 65, 66, 67).join("|")}|"
+    puts "6 |#{@board.fetch_values(60, 61, 62, 63, 64, 65, 66, 67).join("|")}|"
     puts "   _______________________________"
-    puts "5 |#{@hash.fetch_values(50, 51, 52, 53, 54, 55, 56, 57).join("|")}|"
+    puts "5 |#{@board.fetch_values(50, 51, 52, 53, 54, 55, 56, 57).join("|")}|"
     puts "   _______________________________"
-    puts "4 |#{@hash.fetch_values(40, 41, 42, 43, 44, 45, 46, 47).join("|")}|"
+    puts "4 |#{@board.fetch_values(40, 41, 42, 43, 44, 45, 46, 47).join("|")}|"
     puts "   _______________________________"
-    puts "3 |#{@hash.fetch_values(30, 31, 32, 33, 34, 35, 36, 37).join("|")}|"
+    puts "3 |#{@board.fetch_values(30, 31, 32, 33, 34, 35, 36, 37).join("|")}|"
     puts "   _______________________________"
-    puts "2 |#{@hash.fetch_values(20, 21, 22, 23, 24, 25, 26, 27).join("|")}|"
+    puts "2 |#{@board.fetch_values(20, 21, 22, 23, 24, 25, 26, 27).join("|")}|"
     puts "   _______________________________"
-    puts "1 |#{@hash.fetch_values(10, 11, 12, 13, 14, 15, 16, 17).join("|")}|"
+    puts "1 |#{@board.fetch_values(10, 11, 12, 13, 14, 15, 16, 17).join("|")}|"
     puts "   ________________________________"
-    puts "0 |#{@hash.fetch_values(0, 1, 2, 3, 4, 5, 6, 7).join("|")}|"
+    puts "0 |#{@board.fetch_values(0, 1, 2, 3, 4, 5, 6, 7).join("|")}|"
     puts "   _______________________________"
     puts "    0   1   2   3   4   5   6   7"
   end
--- queen.rb
+++ queen.rb
@@ -1,7 +1,10 @@
 require_relative 'board.rb'
 
-class Queen
-  def initialize
+class Queen<Board
+  @@quantity=0
+  def initialize(board)
+    @@quantity+=1
+    #@board=board
     @moves_ri=[1, 2, 3, 4, 5, 6, 7]
     @moves_le=[-1,-2,-3,-4,-5,-6,-7]
     @moves_up=[10, 20, 30, 40, 50, 60, 70]
@@ -10,60 +13,72 @@ class Queen
     @moves_di_up_le=[9, 18, 27, 36, 45, 54, 63]
     @moves_di_do_ri=[-9, -18, -27, -36, -45, -54, -63]
     @moves_di_do_le=[-11,-22,-33,-44,-55,-66,-77]
+    self.set_start_position(board)
+  end
+
+  def set_start_position(board)
+    if @@quantity==1
+      @position=03
+      @piece_symbol=" Qb"
+    else
+      @position=73
+      @piece_symbol=" Qw"
+    end
+    board[@position]=@piece_symbol
   end
 #below makes an array of all the places a queen can go
 # from her current square-ignoring other pieces
 #quits is start location is not on  board
-  def calculate_moves(start, squares, hash)
-    return unless (0..77).include?(start)
+  def calculate_moves(start_position, squares, board)
+    return unless (0..77).include?(start_position)
     @calculated_moves=[]
     @moves_ri.each do |m|
-      landing=m+start
+      landing=m+start_position
       break if !squares.include?(landing)
       @calculated_moves<<landing
-      break if hash[landing]!="   "
+      break if board[landing]!="   "
       end
     @moves_le.each do |m|
-      landing=m+start
+      landing=m+start_position
       break if !squares.include?(landing)
       @calculated_moves<<landing
-      break if hash[landing]!="   "
+      break if board[landing]!="   "
       end
     @moves_up.each do |m|
-      landing=m+start
+      landing=m+start_position
       break if !squares.include?(landing)
       @calculated_moves<<landing
-      break if hash[landing]!="   "
+      break if board[landing]!="   "
       end
     @moves_do.each do |m|
-      landing=m+start
+      landing=m+start_position
       break if !squares.include?(landing)
       @calculated_moves<<landing
-      break if hash[landing]!="   "
+      break if board[landing]!="   "
       end
     @moves_di_up_ri.each do |m|
-      landing=m+start
+      landing=m+start_position
       break if !squares.include?(landing)
       @calculated_moves<<landing
-      break if hash[landing]!="   "
+      break if board[landing]!="   "
       end
     @moves_di_up_le.each do |m|
-      landing=m+start
+      landing=m+start_position
       break if !squares.include?(landing)
       @calculated_moves<<landing
-      break if hash[landing]!="   "
+      break if board[landing]!="   "
       end
     @moves_di_do_ri.each do |m|
-      landing=m+start
+      landing=m+start_position
       break if !squares.include?(landing)
       @calculated_moves<<landing
-      break if hash[landing]!="   "
+      break if board[landing]!="   "
       end
     @moves_di_do_le.each do |m|
-      landing=m+start
+      landing=m+start_position
       break if !squares.include?(landing)
       @calculated_moves<<landing
-      break if hash[landing]!="   "
+      break if board[landing]!="   "
       end
   end
 
@@ -71,6 +86,21 @@ class Queen
     @calculated_moves
   end
 
+  def position
+    @position
+  end
+
+  def make_move(move, board)
+    if @calculated_moves.include?(move)
+      board[@position]="   "
+      @position=move
+      board[@position]=@piece_symbol
+    else
+      puts "illegal move"
+    end
+  end
+
+#test method to show where the queen can go
   def map_moves(board)
     @calculated_moves.each do |x|
       board[x]=" Q "
@@ -78,12 +108,20 @@ class Queen
   end
 
 end
-
+#set up board
 board=Board.new
-board.create_grid
-queen=Queen.new
-board.test_hash([44])
-queen.calculate_moves(33, board.square_numbers, board.hash)
+board.create_board
+#set first queen positions
+queen=Queen.new(board.board)
 
-queen.map_moves(board.hash)
-board.show_hash
+#display state of board
+board.show_board
+
+queen.calculate_moves(queen.position, board.square_numbers, board.board)
+
+queen.make_move(25, board.board)
+board.show_board
+
+queen.calculate_moves(queen.position, board.square_numbers, board.board)
+queen.make_move(35, board.board)
+board.show_board
